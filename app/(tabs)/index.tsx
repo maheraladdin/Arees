ba6b87427e67480f37b74f31a7fbeeca9df79e53
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2024-02-23 16:47:12 +0200

    Init project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
 import { StyleSheet } from 'react-native';
-
+import {useEffect, useState} from "react";
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("http://192.168.1.18:8081/api/hello").then(res => res.json());
+      setData(res.hello);
+    })()
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>hello {data}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
